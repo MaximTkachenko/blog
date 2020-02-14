ac0d4f05d3ecbb080a5776ffbc6fb3264f36e616
mtkachenko <oblomov86@gmail.com>
2020-02-14 14:32:10 +0100

    renaming

---
+++ _drafts/2020-02-14-trie-vs-startswith.md
@@ -0,0 +1,5 @@
+---
+ layout: post 
+ title: trie-vs-startswith
+ comments: true
+---
---
+++ pretzel-bake.cmd
@@ -0,0 +1,5 @@
+dotnet bin\Pretzel.dll bake
+cd _site
+del /f pretzel-install.cmd
+del /f pretzel-bake.cmd
+del /f pretzel-new.cmd
